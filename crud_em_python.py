9b380a2508316959ad0e8775c3baf635dba36f82
José Anderson <jose.andersonjr2@gmail.com>
2022-08-15 13:10:52 -0300

    Finzalização do estudo, teste de CREATE, READ, UPDATE E DELETE

--- crud_em_python.py
+++ crud_em_python.py
@@ -1,4 +1,4 @@
-import mysql
+import mysql.connector
 
 #CONECTANDO AO BANCO DE DADOS E FAZENDO AS CONFIGURAÇÕES INCIAIS
 
@@ -10,10 +10,37 @@ conexao = mysql.connector.connect(
 )
 
 cursor = conexao.cursor()
-comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ()'
 
-# CREATE
+#CREATE, INSERINDO DADOS NA TABELA
+#nome = 'Chocolate'
+#valor = '15'
+#comando = f"INSERT INTO vendas (nome_produto, valor) VALUES ('{nome}', '{valor}')"
+#cursor.execute(comando)
+#conexao.commit() #Comando usado todas as vezes em que há edição no banco de dados
 
 
+#READ, lendo dados das tabelas
+
+comando = 'SELECT * FROM vendas'
+cursor.execute(comando)
+resultado = cursor.fetchall() #Faz a leitura dos dados do banco e traz para o código
+print(resultado)
+
+
+#UPDATE
+valor = '7'
+nome = 'Farinha'
+comando = f"UPDATE vendas SET valor = '{valor}' WHERE nome_produto = '{nome}'"
+cursor.execute(comando)
+conexao.commit()
+
+
+#DELETE
+comando = f"DELETE FROM vendas WHERE nome_produto = 'Chocolate'"
+cursor.execute(comando)
+conexao.commit()
+
+
+#DESLIGANDO A CONEXÃO COM O BANCO
 cursor.close()
 conexao.close()
